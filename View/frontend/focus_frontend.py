f25082d39d31f4d6ba46aa955a5f22023a317396
fedacking <fedacking@gmail.com>
2020-07-03 15:15:15 -0300

    cambios en el daqserver

--- View/frontend/focus_frontend.py
+++ View/frontend/focus_frontend.py
@@ -9,7 +9,7 @@ from View.localization import locale
 
 class FocusFrontend(Frontend):
     gui = ("UI", "focus_control.ui")
-    focus_backend: FocusBackend = InstrumentSlot
+    backend: FocusBackend
 
     def setupUi(self):
         self.widget.control_focus_cb.setText(locale.get("control_focus", "str_control_focus"))
@@ -20,13 +20,17 @@ class FocusFrontend(Frontend):
         self.widget.probe_current_lb.setText(locale.get("probe_current_percent", "str_probe_current_percent"))
         self.widget.probe_current_cb.setText(locale.get("on_off", "str_on_off"))
 
+        self.widget.efocus_chart_lb.setText(locale.get("efocus", "str_efocus"))
+        self.widget.sum_chart_lb.setText(locale.get("sum", "str_sum"))
+        self.widget.reflectance_chart_lb.setText(locale.get("reflectance", "str_reflectance"))
+
         self.widget.focus_light_status.setText(locale.get("focus", "str_focus"))
         self.widget.focus_light_status.setAttribute(Qt.WA_TransparentForMouseEvents)
         self.widget.focus_light_status.setFocusPolicy(Qt.NoFocus)
 
     def connect_backend(self):
 
-        connect_feat(self.widget.focus_light_status, self.focus_backend, "focus_status")
+        connect_feat(self.widget.focus_light_status, self.backend, "focus_status")
 
         series = QLineSeries()
         series.setName(locale.get("amplitude_mv", "str_amplitude_mv"))
