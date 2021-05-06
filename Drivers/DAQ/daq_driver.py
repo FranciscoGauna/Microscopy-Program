79065bc9bfba29b5fe25a47499760ca6904cbbb6
fedacking <fedacking@gmail.com>
2021-05-05 21:36:14 -0300

    merge

--- Drivers/DAQ/daq_driver.py
+++ Drivers/DAQ/daq_driver.py
@@ -2,18 +2,31 @@ from time import sleep
 
 import comtypes
 import comtypes.client
+import threading
+import pause
+from datetime import datetime, timedelta
 
-from lantz import Driver
+pause.until(datetime(2015, 8, 12, 2))
+
+from lantz import Driver, Feat
 from lantz.qt import Backend
 
 
 class ComDAQ(Driver):
+    """
+    This class provides a wrapper for the functions on the ddl for the DAQ com server. This is necessary because the
+    library is only available in 32 bits and this allows us to call it in a 64 process using the processs intercom from
+    windows
+    """
+    device_name = "DaqBoard3K0"
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        self.started = False
         program_id = comtypes.GUID("{DB9935C1-19C5-4ED2-ADD2-9A57E19F53A6}")
         self.lib = comtypes.client.CreateObject(program_id)
+        self._scan_rate = 100
 
     def __del__(self):
         self.lib.StopScanning()
@@ -21,27 +34,71 @@ class ComDAQ(Driver):
 
     def initialize(self):
         super().initialize()
-        self.lib.SetDevice("DaqBoard3K0")
+        self.lib.SetDevice(self.device_name)
         self.lib.OpenDevice()
-        self.lib.SetAnalogInput(5)
-        self.lib.SetAnalogOutput(268435456, 2)
-        self.lib.SetAnalogOutputWave(268435457, 2, 1000 , "sin")
-        self.lib.StartScanning(100, 1000)
-        sleep(1)
+
+    def start_scan(self):
+        self.started = True
+        self.lib.StartScanning(self._scan_rate, 1000)
+
+    @Feat
+    def scan_rate(self):
+        return self._scan_rate
+
+    @scan_rate.setter
+    def scan_rate(self, value):
+        self._scan_rate = value
+
+    def set_analog_input(self, channel):
+        if self.started:
+            return
+        self.lib.SetAnalogInput(channel)
+
+    def set_analog_output(self, channel):
+        self.lib.SetAnalogOutput(channel)
 
     def write_analog(self, port, value):
         print(self.lib.WriteAPort(port, value))
 
-    def read_analog(self, port):
-        print(self.lib.ReadAPort(port))
+    def read_analog(self):
+        """
+        read the analog inputs that were set up before started scannign
+        :return:
+        """
+        results = self.lib.ReadAPort().split(";")
+        return map(float, results)
 
 
 class ComDaqBackend(Backend):
+    """
+    This class provides the backend functions necessary to operate the daq inside the program
+    """
     daq: ComDAQ
+    timer: threading.Thread
+    lock = threading.Lock()
+    data: list
+    started = True
+    time: datetime
+    delt: timedelta
+
+    def focus(self):
+        return True
+
+    def run(self):
+        while self.started:
+            self.lock.acquire()
+            self.data = [x for x in self.daq.read_analog()]
+            self.lock.acquire()
+            self.time += self.delta
+            pause.until(self.time)
 
     def __init__(self):
         super().__init__()
         self.daq = ComDAQ()
-
-    def focus(self):
-        return True
+        self.daq.initialize()
+        self.data = []
+        self.timer = threading.Thread(target=self.run)
+        self.time = datetime.now()
+        self.delta = timedelta(seconds=(1 / self.daq.scan_rate))
+        self.daq.start_scan()
+        self.timer.start()
